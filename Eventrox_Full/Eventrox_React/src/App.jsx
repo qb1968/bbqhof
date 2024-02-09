f190430b4561cda6d05892537ddfe6c7ba68ca41
qb1968 <mark.allison75@gmail.com>
2024-02-09 07:58:24 -0500

    one more thing

--- Eventrox_Full/Eventrox_React/src/App.jsx
+++ Eventrox_Full/Eventrox_React/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       {isLoading ? <Preloader /> :
         <div className='app-wrapper'>
           <Helmet>
-            <title>Eventrox - Digital Events React Template</title>
+            <title>NCBBQHOF</title>
             <link rel="shortcut icon" href="../images/favicon.ico"></link>
           </Helmet>
           <Routers />
