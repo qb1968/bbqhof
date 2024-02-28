9b6b2c5cd67dc5702cb76a317ad3405f3df667f9
qb1968 <mark.allison75@gmail.com>
2024-02-28 14:52:55 -0500

    gallery and bio update

--- Eventrox_Full/Eventrox_React/src/components/gallery/GalleryPageContent.jsx
+++ Eventrox_Full/Eventrox_React/src/components/gallery/GalleryPageContent.jsx
@@ -43,9 +43,18 @@ const GalleryPageContent = () => {
             </div>
           </div>
         </section> */}
-        <section className="team-area fix p-relative pt-120 pb-90">
-          <div className="container" style={{ padding: 20 }}>
-            <h2 style={{color:"red",fontWeight:"bold"}}>Alston Bridges Barbecue</h2>
+        <section className="team-area fix p-relative pt-100 pb-50">
+          <div
+            className="container"
+            style={{
+              padding: 20,
+              marginBottom: 70,
+              borderBottom: "1rem solid black",
+            }}
+          >
+            <h2 style={{ color: "black", fontWeight: "bold" }}>
+              Alston Bridges Barbecue
+            </h2>
             <div className="row">
               <div className="col-lg-4">
                 <figure>
@@ -53,19 +62,15 @@ const GalleryPageContent = () => {
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
                   <img src={`../images/resource/ab1.1.png`} alt="" />
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
-                  <img
-                    src={`../images/resource/ab3.png`}
-                    alt=""
-                    style={{ width: 300, height: 500 }}
-                  />
+                  <img src={`../images/resource/ab3.png`} alt="" />
                 </figure>
               </div>
 
@@ -74,15 +79,20 @@ const GalleryPageContent = () => {
                   <img src={`../images/resource/ab5.png`} alt="" />
                 </figure>
               </div>
-              <div className="col-lg-3">
+              <div className="col-lg-6">
                 <img src={`../images/resource/ab4.png`} alt="" />
               </div>
             </div>
           </div>
-          <div className="container" style={{ padding: 20 }}>
-         
-            
-            <h2>Grady's BBQ</h2>
+          <div
+            className="container"
+            style={{
+              padding: 20,
+              marginBottom: 70,
+              borderBottom: "1rem solid black",
+            }}
+          >
+            <h2 style={{ color: "black", fontWeight: "bold" }}>Grady's BBQ</h2>
             <div className="row">
               <div className="col-lg-4">
                 <figure>
@@ -90,18 +100,18 @@ const GalleryPageContent = () => {
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
                   <img src={`../images/resource/gradys2.jpg`} alt="" />
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
                   <img
                     src={`../images/resource/gradys3.jpg`}
                     alt=""
-                    style={{ width: 300, height: 500 }}
+                    
                   />
                 </figure>
               </div>
@@ -120,44 +130,53 @@ const GalleryPageContent = () => {
             </div>
           </div>
 
-          <div className="container" style={{ padding: 10 }}>
-            <h2>Hursey's Bar-B-Q</h2>
+          <div
+            className="container"
+            style={{
+              padding: 20,
+              marginBottom: 70,
+              borderBottom: "1rem solid black",
+            }}
+          >
+            <h2 style={{ color: "black", fontWeight: "bold" }}>
+              Hursey's Bar-B-Q
+            </h2>
             <div className="row">
+              <div className="col-lg-2">
+                <figure>
+                  <img src={`../images/resource/hbq15.jpg`} alt="" />
+                </figure>
+              </div>
+
               <div className="col-lg-4">
                 <figure>
-                  <img src={`../images/resource/hbq.jpg`} alt="" />
+                  <img src={`../images/resource/hbq27.jpg`} alt="" />
                 </figure>
               </div>
 
-              <div className="col-lg-6">
-                <figure>
-                  <img src={`../images/resource/hbq2.jpg`} alt="" />
-                </figure>
-              </div>
-
-              <div className="col-lg-6">
+              <div className="col-lg-2">
                 <figure>
                   <img
                     src={`../images/resource/hbq4.jpg`}
                     alt=""
-                    style={{ width: 300, height: 500 }}
+                    // style={{ width: 300, height: 500 }}
                   />
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
-                  <img src={`../images/resource/hbq5.jpg`} alt="" />
+                  <img src={`../images/resource/hbq8.jpg`} alt="" />
                 </figure>
               </div>
-              <div className="col-lg-3">
+              <div className="col-lg-4">
                 <img src={`../images/resource/hbq6.jpg`} alt="" />
               </div>
               <div className="col-lg-4">
                 <img src={`../images/resource/hbq7.jpg`} alt="" />
               </div>
-              <div className="col-lg-4">
-                <img src={`../images/resource/hbq8.jpg`} alt="" />
+              <div className="col-lg-2">
+                <img src={`../images/resource/hbq25.jpg`} alt="" />
               </div>
               <div className="col-lg-4">
                 <img src={`../images/resource/hbq9.jpg`} alt="" />
@@ -166,20 +185,29 @@ const GalleryPageContent = () => {
                 <img src={`../images/resource/hbq10.jpg`} alt="" />
               </div>
               <div className="col-lg-4">
-                <img src={`../images/resource/hbq15.jpg`} alt="" />
+                <img src={`../images/resource/hbq1.jpg`} alt="" />
               </div>
               <div className="col-lg-4">
-                <img src={`../images/resource/hbq14.jpg`} alt="" />
+                <img src={`../images/resource/hbq.jpg`} alt="" />
               </div>
-              <div className="col-lg-4">
+              <div className="col-lg-8">
                 <img src={`../images/resource/hbq11.jpg`} alt="" />
               </div>
             </div>
           </div>
-          <div className="container" style={{ padding: 10 }}>
-            <h2>Lexington Barbecue</h2>
+          <div
+            className="container"
+            style={{
+              padding: 20,
+              marginBottom: 70,
+              borderBottom: "1rem solid black",
+            }}
+          >
+            <h2 style={{ color: "black", fontWeight: "bold" }}>
+              Lexington Barbecue
+            </h2>
             <div className="row">
-              <div className="col-lg-4">
+              <div className="col-lg-6">
                 <figure>
                   <img src={`../images/resource/lb1.jpg`} alt="" />
                 </figure>
@@ -196,7 +224,7 @@ const GalleryPageContent = () => {
                   <img
                     src={`../images/resource/lb3.jpg`}
                     alt=""
-                    style={{ width: 300, height: 500 }}
+                   
                   />
                 </figure>
               </div>
@@ -206,16 +234,24 @@ const GalleryPageContent = () => {
                   <img src={`../images/resource/lb5.jpg`} alt="" />
                 </figure>
               </div>
-              <div className="col-lg-3">
+              <div className="col-lg-6">
                 <img src={`../images/resource/lb4.jpg`} alt="" />
               </div>
-             
             </div>
           </div>
-          <div className="container" style={{ padding: 10 }}>
-            <h2>Sam Jones BBQ</h2>
+          <div
+            className="container"
+            style={{
+              padding: 20,
+              marginBottom: 70,
+              borderBottom: "1rem solid black",
+            }}
+          >
+            <h2 style={{ color: "black", fontWeight: "bold" }}>
+              Sam Jones BBQ
+            </h2>
             <div className="row">
-              <div className="col-lg-4">
+              <div className="col-lg-6">
                 <figure>
                   <img src={`../images/resource/sj1.jpg`} alt="" />
                 </figure>
@@ -242,36 +278,44 @@ const GalleryPageContent = () => {
                   <img src={`../images/resource/sj4.jpg`} alt="" />
                 </figure>
               </div>
-              <div className="col-lg-3">
+              <div className="col-lg-6">
                 <img src={`../images/resource/sj5.jpg`} alt="" />
               </div>
-              <div className="col-lg-3">
+              <div className="col-lg-6">
                 <img src={`../images/resource/sj6.jpg`} alt="" />
               </div>
-             
             </div>
           </div>
-          <div className="container" style={{ padding: 10 }}>
-            <h2>Skylight Inn BBQ</h2>
+          <div
+            className="container"
+            style={{
+              padding: 20,
+              marginBottom: 70,
+              borderBottom: "1rem solid black",
+            }}
+          >
+            <h2 style={{ color: "black", fontWeight: "bold" }}>
+              Skylight Inn BBQ
+            </h2>
             <div className="row">
-              <div className="col-lg-4">
+              <div className="col-lg-6">
                 <figure>
                   <img src={`../images/resource/si1.jpg`} alt="" />
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
                   <img src={`../images/resource/si2.jpg`} alt="" />
                 </figure>
               </div>
 
-              <div className="col-lg-6">
+              <div className="col-lg-4">
                 <figure>
                   <img
                     src={`../images/resource/si3.jpg`}
                     alt=""
-                    style={{ width: 300, height: 500 }}
+                    
                   />
                 </figure>
               </div>
@@ -287,43 +331,41 @@ const GalleryPageContent = () => {
               <div className="col-lg-3">
                 <img src={`../images/resource/si6.jpg`} alt="" />
               </div>
-             
             </div>
           </div>
           <div className="container" style={{ padding: 10 }}>
-            <h2>Stamey's Barbecue</h2>
+            <h2 style={{ color: "black", fontWeight: "bold" }}>
+              Stamey's Barbecue
+            </h2>
             <div className="row">
+              <div className="col-lg-6">
+                <figure>
+                  <img src={`../images/resource/Warner in Pit.jpg`} alt="" />
+                </figure>
+              </div>
+
+              <div className="col-lg-6">
+                <figure>
+                  <img src={`../images/resource/Warner 3rd from left.jpg`} alt="" />
+                </figure>
+              </div>
+
               <div className="col-lg-4">
                 <figure>
-                  <img src={`../images/resource/sb1.jpg`} alt="" />
-                </figure>
-              </div>
-
-              <div className="col-lg-6">
-                <figure>
-                  <img src={`../images/resource/sb2.jpg`} alt="" />
-                </figure>
-              </div>
-
-              <div className="col-lg-6">
-                <figure>
                   <img
-                    src={`../images/resource/sb3.jpg`}
+                    src={`../images/resource/IMG_1215.jpg`}
                     alt=""
-                    style={{ width: 300, height: 500 }}
+                    
                   />
                 </figure>
               </div>
 
+              
               <div className="col-lg-6">
-                <figure>
-                  <img src={`../images/resource/sb4.jpg`} alt="" />
-                </figure>
+                <figure><img src={`../images/resource/IMG_1648.jpg`} alt="" /></figure>
+                
               </div>
-              <div className="col-lg-3">
-                <img src={`../images/resource/sb5.jpg`} alt="" />
-              </div>
-              </div>
+            </div>
           </div>
         </section>
       </>
